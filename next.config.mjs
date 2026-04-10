7f237bfa622ffa34389fb8c49f095aa28484423d
gjiawei <goh.jiawei@illumina.com>
2026-04-10 22:20:34 +0800

    fix localhost vs ghpages route

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/Nihon-go',
+  basePath: process.env.NODE_ENV === 'production' ? '/Nihon-go' : '',
   images: {
     unoptimized: true,
   },
